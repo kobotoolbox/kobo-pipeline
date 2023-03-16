35653b74adee3dcaca8089a17ebbb6e7011f5b75
Alex Dorey <dorey415@gmail.com>
2023-03-16 14:50:31 -0400

    make columns configurable with a JSON string

--- constants.js
+++ constants.js
@@ -1,3 +1,53 @@
+let ExampleColumnMappingJson = `
+{
+  "ID_OF_PARTICIPANT": "ID de participant",
+  "NAME": "Prénom",
+  "RECRUITED_BY_ID": "Recruté par",
+  "PHONE_NUMBER_COL": "Numéro de téléphone",
+  "RECRUITS_COL": "Recrues",
+  "CARRIER": "Airtable COL CARRIER",
+  "INCENTIVE": "Airtable COL TEL INCENTIVO",
+  "REFS_CARRIER": "Airtable COL REFS CARRIER",
+  "REFS_INCENTIVE": "Airtable COL REFS INCENTIVE",
+  "SPECIFIED_CARRIER": "SPECIFIED_CARRIER",
+  "PHONE_FOR_INCENTIVE": "PHONE_FOR_INCENTIVE",
+  "TIMESTAMP": "Date invitation",
+  "UUID": "submission_uuid",
+  "PARENT_UUID": "Airtable COL submission_uuid"
+}
+`;
+
+let AT = JSON.parse(
+  process.env.COL_MAPPING ? process.env.COL_MAPPING : ExampleColumnMappingJson
+);
+
+// assert expected keys are found
+[
+  'ID_OF_PARTICIPANT',
+  'NAME',
+  'RECRUITED_BY_ID',
+  'PHONE_NUMBER_COL',
+  'RECRUITS_COL',
+  'CARRIER',
+  'INCENTIVE',
+  'REFS_CARRIER',
+  'REFS_INCENTIVE',
+  'SPECIFIED_CARRIER',
+  'PHONE_FOR_INCENTIVE',
+  'TIMESTAMP',
+  'UUID',
+  'PARENT_UUID',
+].forEach((key) => {
+  if (!AT[key]) {
+    throw new Error(`Missing Config: ${key}`)
+  }
+});
+
+Object.assign(AT, {
+  VIEW_NAME: process.env.AT_VIEW_NAME || 'All data',
+  TABLE_NAME: process.env.AT_TABLE_NAME || 'Respondent tracker',
+});
+
 module.exports = {
   // field names pulled from the kobo submission
   KOBODATA: {
@@ -12,22 +62,5 @@ module.exports = {
     UUID_PATH: 'meta/instanceID',
   },
   // Fields used in the column headers of the AirTable (AT) document
-  AT: {
-    VIEW_NAME: process.env.AT_VIEW_NAME || 'All data',
-    TABLE_NAME: process.env.AT_TABLE_NAME || 'Respondent tracker',
-    ID_OF_PARTICIPANT: 'ID de participant',
-    NAME: 'Prénom',
-    RECRUITED_BY_ID: 'Recruté par',
-    PHONE_NUMBER_COL: 'Numéro de téléphone',
-    RECRUITS_COL: 'Recrues',
-    CARRIER: 'Airtable COL CARRIER',
-    INCENTIVE: 'Airtable COL TEL INCENTIVO',
-    REFS_CARRIER: 'Airtable COL REFS CARRIER',
-    REFS_INCENTIVE: 'Airtable COL REFS INCENTIVE',
-    SPECIFIED_CARRIER: 'SPECIFIED_CARRIER',
-    PHONE_FOR_INCENTIVE: 'PHONE_FOR_INCENTIVE',
-    TIMESTAMP: 'Date invitation',
-    UUID: 'submission_uuid',
-    PARENT_UUID: 'Airtable COL submission_uuid',
-  },
+  AT,
 }
